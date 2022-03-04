42117eab219013c5570f61917a255d1128e61d75
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-03-04 12:14:03 +0100

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,15 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+      ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "rules": {
+        "react/react-in-jsx-scope": false
     }
 }
