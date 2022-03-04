a41eb5cce4fb25a0396dc8c13b24baaa587054fe
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-03-04 16:42:57 +0100

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [shoppingItems, setShoppingItems] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [shoppingListItems, setShoppingListItems] = useState([]);
+
+  useEffect(() => {
+    loadItems();
+  }, []);
+  async function loadItems() {
+    try {
+      const response = await fetch(
+        'https://fetch-me.vercel.app/api/shopping/items'
+      );
+      const data = await response.json();
+      setShoppingItems(data.data);
+      console.log(shoppingItems);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleChange(event) {
+    setSearchValue(event.target.value);
+  }
+  function handleClick(event) {
+    setShoppingListItems([event.target.value, ...shoppingListItems]);
+    console.log(event);
+  }
   return (
     <main>
-      <h1>Hello World!</h1>
+      <input onChange={handleChange}></input>
+      <div>
+        {searchValue
+          ? shoppingItems
+              .filter(({ name }) => name.de.startsWith(searchValue))
+              .map(({ name, _id }) => (
+                <button onClick={handleClick} key={_id}>
+                  {name.de}
+                </button>
+              ))
+          : null}
+      </div>
+      <h2>recently used</h2>
+      {/* <ul>
+        {shoppingListItems
+          ? shoppingListItems.map(({ name, _id }) => (
+              <button key={_id}>{name.de}</button>
+            ))
+          : null}
+      </ul> */}
     </main>
   );
 }
